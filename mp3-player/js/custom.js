3a76d80266a3c90bf9a532e5ff158fa83cd55d09
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-02-01 13:34:40 +0300

    up

--- mp3-player/js/custom.js
+++ mp3-player/js/custom.js
@@ -5,25 +5,32 @@ function play() {
   if (audio[0].paused) {
     audio[0].play();
 
+  
+// const timee = audio[0].currentTime;
+    
     time();
+
   } else {
     audio[0].pause();
   }
 }
 function time() {
-
   var audio_time = document.getElementById("music");
   var time_split = audio_time.currentTime;
   var time = time_split.toString().split(".", 1);
   const time1 = time.map(Number);
-
-
-
-
-time1.forEach(function (e) {
-
-    console.log(e);
-    $(".music-bar-width").css("width", e,"%")
+  
+  
+  
+  
+  time1.forEach(function (e) {
+   setInterval(() => {
+  
+    $(".music-bar-width").css("width", e++,"%")
+   }, 1000);
+   
 });
 
+
 }
+
